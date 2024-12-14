0a9bf5016f532e1d021903f5f8603ba210a799f5
hannutt <htuomela@gmail.com>
2024-12-14 04:10:50 +0200

    self-writing postgre query feature added

--- postgreScripts.py
+++ postgreScripts.py
@@ -1,11 +1,11 @@
 import psycopg2
 from flask import Blueprint, render_template, request
-
+showtables=False
 postgreScripts = Blueprint('postgreScripts',__name__,static_folder='static',template_folder='templates')
 
 def connection():
 
-    conn = psycopg2.connect(database="stuffdb",
+    conn = psycopg2.connect(database="",
                             
                             host="localhost",
                             user="postgres",
@@ -13,11 +13,13 @@ def connection():
                             port=5433)
     return conn
 
+
 @postgreScripts.route("/postgresql",methods=['POST','GET'])
 def readPostgre():
     databases=[]
     conn=connection()
     cursor = conn.cursor()
+    #haetaan kaikki tietokannat
     cursor.execute("SELECT datname FROM pg_database;")
     for i in cursor.fetchall():
         print(i)
@@ -25,4 +27,71 @@ def readPostgre():
    
 
     return render_template("postgresql.html",databases=databases)
+
+@postgreScripts.route("/postgretables",methods=['POST','GET'])
+def getTables():
+    showtables=True
+    tables=[]
+    #talletetaan valitun tietokannan nimi 
+    global db
+    db=request.form.get('selectedPostgre')
+    conn = psycopg2.connect(database=db,
+                            
+                            host="localhost",
+                            user="postgres",
+                            password="SkaneBorg12!",
+                            port=5433)
+    cursor = conn.cursor()
+    #haetaan kaikki taulut valitusta tietokannasta.
+    cursor.execute(f"""
+    SELECT table_name
+    FROM information_schema.tables
+    WHERE table_schema = 'public'
+""")
+    for table in cursor.fetchall():
+        print(table)
+        tables.append(table)
+    return render_template("postgresql.html",showtables=showtables,tables=tables)
+
+@postgreScripts.route("/showpostgretable",methods=['POST','GET'])
+def showPostgreTable():
+    tabledata=[]
+    columnname=[]
+ 
+    conn = psycopg2.connect(database=db,
+                            host="localhost",
+                            user="postgres",
+                            password="SkaneBorg12!",
+                            port=5433)
+    global tablename
+    tablename=request.form.get("postgreTable")
+    cursor=conn.cursor()
+    
+    cursor.execute(f"SELECT * FROM {tablename}")
+    #sarakkeiden otsikot
+    column_names = [desc[0] for desc in cursor.description] 
+    for i in cursor.fetchall():
+        tabledata.append(i)
+    for j in column_names:
+        columnname.append(j)
+    return render_template("postgresql.html",tabledata=tabledata,columnname=columnname)
+
+@postgreScripts.route("/writepostgre",methods=['POST','GET'])
+def writePostgreQuery():
+     tabledata=[]
+     conn = psycopg2.connect(database=db,
+                            host="localhost",
+                            user="postgres",
+                            password="SkaneBorg12!",
+                            port=5433)
+     
+     query=request.form.get("writePostgre")
+     cursor=conn.cursor()
+     cursor.execute(query)
+     for c in cursor.fetchall():
+         tabledata.append(c)
+     return render_template("postgresql.html",tabledata=tabledata)
+
+    
+
 
--- static/js/postgreScripts.js
+++ static/js/postgreScripts.js
@@ -1,3 +1,30 @@
 function goPostgrePage() {
-    document.getElementById("gobtn").click()
+    var cb = document.getElementById("postgreCB").checked
+    if (cb) {
+        document.getElementById("gobtn").click()
+
+    }
+}
+
+function getPostgreDB(database) {
+    var sel = document.getElementById("postgre").value = database.options[database.selectedIndex].text
+    var cleartext = sel.replace("(", "").replace(")", "").replace("'", "").replace("'", "").replace(",", "")
+    document.getElementById("selectedPostgre").value = cleartext
+    
+}
+function getPostgeTable(table) {
+    var sel = document.getElementById("postgreTables").value = table.options[table.selectedIndex].text
+    var cleartext = sel.replace("(", "").replace(")", "").replace("'", "").replace("'", "").replace(",", "")
+    document.getElementById("postgreTable").value = cleartext
+
+
+}
+function writePostgre() {
+    var cb=document.getElementById("postgreQueryCB" ).checked
+    if (cb) {
+        document.getElementById("writePostgre").hidden=false
+    }
+    else{
+        document.getElementById("writePostgre").hidden=true
+    }
 }
