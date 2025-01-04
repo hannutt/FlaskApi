1fb4d738b9ee7d1fa3c6e2fd15c43319734b80cb
hannutt <htuomela@gmail.com>
2025-01-04 04:34:42 +0200

    improvements to postgresql self-writing queries

--- postgreScripts.py
+++ postgreScripts.py
@@ -1,5 +1,5 @@
 import psycopg2
-from flask import Blueprint, render_template, request
+from flask import Blueprint, redirect, render_template, request
 showtables=False
 postgreScripts = Blueprint('postgreScripts',__name__,static_folder='static',template_folder='templates')
 tableSelected=False
@@ -70,6 +70,7 @@ def showPostgreTable(db):
     postgredb=db
     tableSelected=True
     tabledata=[]
+    global columnname
     columnname=[]
  
     conn = psycopg2.connect(database=db,
@@ -86,6 +87,7 @@ def showPostgreTable(db):
     column_names = [desc[0] for desc in cursor.description] 
     for i in cursor.fetchall():
         tabledata.append(i)
+    #sarakeiden nimet
     for j in column_names:
         columnname.append(j)
     return render_template("postgresql.html",tabledata=tabledata,columnname=columnname,tableSelected=tableSelected)
@@ -101,10 +103,71 @@ def writePostgreQuery():
      
      query=request.form.get("writePostgre")
      cursor=conn.cursor()
-     cursor.execute(query)
-     for c in cursor.fetchall():
-         tabledata.append(c)
+     
+     #jos query alkaa sanalla insert toteutaan commit funktio, joka tallentaan datan kantaan
+     if query.startswith("INSERT") or query.startswith("insert"):
+        cursor.execute(query)
+        conn.commit()
+     else:
+        cursor.execute(query)
+        for c in cursor.fetchall():
+             tabledata.append(c)
+    
+    
      return render_template("postgresql.html",tabledata=tabledata)
+
+@postgreScripts.route("/postgrecrud", methods=['POST','GET'])
+def postgreCrud():
+    data=[]
+    fields=request.form.get("fieldsNum")
+    fieldsInt=int(fields)
+    #jos on painettu buttonia, jonka value on editBtn
+    editBtn = request.form.get('editBtn')
+    delBtn=request.form.get("delBtn")
+  
+    if editBtn:
+         for i in range(0,fieldsInt):
+             #kierrosmuuttuja täytyy muuntaa merkkijonoksi koska input kentän name attribuutti on myös merkkijono
+             #tyyppinen.
+             i=str(i)
+             j=request.form.get(i)
+             data.append(j)
+         print(data)
+         print(columnname)           
+         
+        
+         conn = psycopg2.connect(database=postgredb,
+                            host="localhost",
+                            user="postgres",
+                            password="SkaneBorg12!",
+                            port=5433)
+         conn = cursor=conn.cursor()
+         sql=f"UPDATE {tablename} set model={data[2]} WHERE id=1"
+         cursor.execute(sql)
+         conn.close()
+         
+    if delBtn:
+        print("deleting")
+    
+    return render_template("postgresql.html")
+
+@postgreScripts.route("/postgresqldelete", methods=['POST','GET'])
+def deletePostgreRecord():
+    id=request.form.get("id")
+    idToInt=int(id)
+ 
+    conn = psycopg2.connect(database=postgredb,
+                            host="localhost",
+                            user="postgres",
+                            password="SkaneBorg12!",
+                            port=5433)
+    sql=f"DELETE FROM {tablename} WHERE ID ={idToInt}"
+    cursor=conn.cursor()
+    cursor.execute(sql)
+    conn.commit()
+    conn.close()
+    return render_template("postgresql.html")
+
 
     
 
